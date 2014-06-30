ea18d51d9a23ea5cac81f82a88c2dd99c349e4ea
MoeBoy76 <moeboy76@gmail.com>
2014-06-30 22:35:09 +1000

    this is it

---
+++ src/main/java/com/pixelutilitys/items/SaphireItem.java
@@ -0,0 +1,13 @@
+package com.pixelutilitys.items;
+
+import net.minecraft.item.Item;
+public class SaphireItem extends Item {
+
+        public SaphireItem() {
+                super();
+                
+                // Constructor Configuration
+                maxStackSize = 64;
+                setUnlocalizedName("SaphireItem");
+        }
+}
